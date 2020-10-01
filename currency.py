f675afe7fa1597406778b59f863eba3228db4b5a
Alex <alexcheung880@Gmail.com>
2020-09-30 22:00:07 -0700

    Update currency.py

---
+++ currency.py
@@ -0,0 +1,14 @@
+# currency exchange calculator 
+# based on approximate set values 
+# values used were collected for the date: 
+# October 1, 2020
+
+# convert 1 USD to -> 
+'''
+Australian Dollar: 1.4003
+Brazilian Real: 5.6274
+British Pound: 0.77683
+Canadian Dollar: 1.33632
+Chinese Yuan: 6.80127
+
+'''
