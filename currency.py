709605d6bb4410b3a4ac9100b84f406deef68068
Alex <alexcheung880@Gmail.com>
2020-10-04 01:12:17 -0700

    Update currency.py

--- currency.py
+++ currency.py
@@ -6,8 +6,8 @@
 # values from: 
 # https://www1.oanda.com/currency/comparison
 
+# values for converting
 # convert 1 USD to -> 
-'''
 australian_dollar = 1.4003
 brazilian_real = 5.6274
 british_pound = 0.77683
@@ -34,46 +34,55 @@ taiwan_dollar = 28.95307
 thai_baht = 31.59357
 turkish_lira = 7.75733
 us_dollar = 1
-'''
 
-'''
-user_input = float(input("How many USD to TWD? "))
-TWD = 28.95307
-output = user_input * TWD
-output = round(output, 2)     # change decimal to two points
-print(str(user_input) + " USD was converted to: " + str(output) + " TWD. \nThank you for using our service.")
-'''
+
+def main(): 
+    que = int(input("Helo, please enter a number of what you would like to do today: \n1) exchange currencies \n2) view exchange rates \n"))
+    if que == 1:
+        pass
+    elif que == 2:
+        print_currencies()
+
+    '''
+    user_input = float(input("How many USD to TWD? "))
+    TWD = 28.95307
+    output = user_input * TWD
+    output = round(output, 2)     # change decimal to two points
+    print(str(user_input) + " USD was converted to: " + str(output) + " TWD. \nThank you for using our service.")
+    '''
+
 
 # when called upon print list of available currencies and values
 def print_currencies(): 
-    print('\033[1m' + '\033[92m' + "CURRENCIES AVAILABLE TO EXCHANGE: " + '\033[0m' + 
-    """
-    AUD     Australian Dollar: 1.4003
-    BRL     Brazilian Real: 5.6274
-    GBP     British Pound: 0.77683
-    CAD     Canadian Dollar: 1.33632
-    CNY     Chinese Yuan: 6.80127
-    DKK     Danish Krone: 6.3495
-    EUR     Euro: 0.85284
-    HKD     Hong Kong Dollar: 7.74988
-    HUF     Hungarian Forint: 310.68414
-    INR     Indian Rupee: 73.56519
-    IDR     Indonesian Rupiah: 14897.42782
-    JPY     Japanese Yen: 105.57228
-    MXN     Mexican Peso: 22.25874
-    NZD     New Zealand Dollar: 1.51507
-    NOK     Norwegian Kroner: 9.40294
-    PLN     Polish Zloty: 3.86343
-    RUB     Russian Rouble: 78.24798
-    SAR     Saudi Riyal: 3.74933
-    SGD     Singapore Dollar: 1.36727
-    ZAR     South African Rand: 16.82467
-    SEK     Swedish Krona: 8.97499
-    CHF     Swiss Franc: 0.92104
-    TWD     Taiwan Dollar: 28.95307
-    THB     Thai Baht: 31.59357
-    TRY     Turkish Lira: 7.75733
-    USD     US Dollar: 1
-    """
+    print("\n" + '\033[1m' + '\033[92m' + "Currency Exchange Rates: " + '\033[0m' + 
+  """
+  AUD     Australian Dollar: 1.4003
+  BRL     Brazilian Real: 5.6274
+  GBP     British Pound: 0.77683
+  CAD     Canadian Dollar: 1.33632
+  CNY     Chinese Yuan: 6.80127
+  DKK     Danish Krone: 6.3495
+  EUR     Euro: 0.85284
+  HKD     Hong Kong Dollar: 7.74988
+  HUF     Hungarian Forint: 310.68414
+  INR     Indian Rupee: 73.56519
+  IDR     Indonesian Rupiah: 14897.42782
+  JPY     Japanese Yen: 105.57228
+  MXN     Mexican Peso: 22.25874
+  NZD     New Zealand Dollar: 1.51507
+  NOK     Norwegian Kroner: 9.40294
+  PLN     Polish Zloty: 3.86343
+  RUB     Russian Rouble: 78.24798
+  SAR     Saudi Riyal: 3.74933
+  SGD     Singapore Dollar: 1.36727
+  ZAR     South African Rand: 16.82467
+  SEK     Swedish Krona: 8.97499
+  CHF     Swiss Franc: 0.92104
+  TWD     Taiwan Dollar: 28.95307
+  THB     Thai Baht: 31.59357
+  TRY     Turkish Lira: 7.75733
+  USD     US Dollar: 1
+  """
     )
-print_currencies()
+
+main()
