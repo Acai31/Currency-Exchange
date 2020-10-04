eb8221d0cdc281090713f5218fc76aaf261eb658
Alex <alexcheung880@Gmail.com>
2020-10-03 18:04:56 -0700

    created function when called upon prints available currencies to exchange

--- currency.py
+++ currency.py
@@ -36,10 +36,44 @@ us_dollar = 1
 
 '''
 
-
-
+'''
 user_input = float(input("How many USD to TWD? "))
 TWD = 28.95307
 output = user_input * TWD
 output = round(output, 2)     # change decimal to two points
 print(str(user_input) + " USD was converted to: " + str(output) + " TWD. \nThank you for using our service.")
+'''
+
+# when called upon print list of available currencies and values
+def print_currencies(): 
+    print('\033[1m' + '\033[92m' + "CURRENCIES AVAILABLE TO EXCHANGE: " + '\033[0m' + 
+    """
+    Australian Dollar: 1.4003
+    Brazilian Real: 5.6274
+    British Pound: 0.77683
+    Canadian Dollar: 1.33632
+    Chinese Yuan: 6.80127
+    Danish Krone: 6.3495
+    Euro: 0.85284
+    Hong Kong Dollar: 7.74988
+    Hungarian Forint: 310.68414
+    Indian Rupee: 73.56519
+    Indonesian Rupiah: 14897.42782
+    Japanese Yen: 105.57228
+    Mexican Peso: 22.25874
+    New Zealand Dollar: 1.51507
+    Norwegian Kroner: 9.40294
+    Polish Zloty: 3.86343
+    Russian Rouble: 78.24798
+    Saudi Riyal: 3.74933
+    Singapore Dollar: 1.36727
+    South African Rand: 16.82467
+    Swedish Krona: 8.97499
+    Swiss Franc: 0.92104
+    Taiwan Dollar: 28.95307
+    Thai Baht: 31.59357
+    Turkish Lira: 7.75733
+    US Dollar: 1
+    """
+    )
+print_currencies()
