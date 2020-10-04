18190e01719b4667e44f41d02126138ecd81cf17
Alex <alexcheung880@Gmail.com>
2020-10-03 18:30:11 -0700

    currency symbols added

--- currency.py
+++ currency.py
@@ -48,32 +48,32 @@ print(str(user_input) + " USD was converted to: " + str(output) + " TWD. \nThank
 def print_currencies(): 
     print('\033[1m' + '\033[92m' + "CURRENCIES AVAILABLE TO EXCHANGE: " + '\033[0m' + 
     """
-    Australian Dollar: 1.4003
-    Brazilian Real: 5.6274
-    British Pound: 0.77683
-    Canadian Dollar: 1.33632
-    Chinese Yuan: 6.80127
-    Danish Krone: 6.3495
-    Euro: 0.85284
-    Hong Kong Dollar: 7.74988
-    Hungarian Forint: 310.68414
-    Indian Rupee: 73.56519
-    Indonesian Rupiah: 14897.42782
-    Japanese Yen: 105.57228
-    Mexican Peso: 22.25874
-    New Zealand Dollar: 1.51507
-    Norwegian Kroner: 9.40294
-    Polish Zloty: 3.86343
-    Russian Rouble: 78.24798
-    Saudi Riyal: 3.74933
-    Singapore Dollar: 1.36727
-    South African Rand: 16.82467
-    Swedish Krona: 8.97499
-    Swiss Franc: 0.92104
-    Taiwan Dollar: 28.95307
-    Thai Baht: 31.59357
-    Turkish Lira: 7.75733
-    US Dollar: 1
+    AUD     Australian Dollar: 1.4003
+    BRL     Brazilian Real: 5.6274
+    GBP     British Pound: 0.77683
+    CAD     Canadian Dollar: 1.33632
+    CNY     Chinese Yuan: 6.80127
+    DKK     Danish Krone: 6.3495
+    EUR     Euro: 0.85284
+    HKD     Hong Kong Dollar: 7.74988
+    HUF     Hungarian Forint: 310.68414
+    INR     Indian Rupee: 73.56519
+    IDR     Indonesian Rupiah: 14897.42782
+    JPY     Japanese Yen: 105.57228
+    MXN     Mexican Peso: 22.25874
+    NZD     New Zealand Dollar: 1.51507
+    NOK     Norwegian Kroner: 9.40294
+    PLN     Polish Zloty: 3.86343
+    RUB     Russian Rouble: 78.24798
+    SAR     Saudi Riyal: 3.74933
+    SGD     Singapore Dollar: 1.36727
+    ZAR     South African Rand: 16.82467
+    SEK     Swedish Krona: 8.97499
+    CHF     Swiss Franc: 0.92104
+    TWD     Taiwan Dollar: 28.95307
+    THB     Thai Baht: 31.59357
+    TRY     Turkish Lira: 7.75733
+    USD     US Dollar: 1
     """
     )
 print_currencies()
