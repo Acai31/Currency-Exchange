7e6a486cea5820d7ba7c6b03e607c1f45c94e90b
Alex <alexcheung880@Gmail.com>
2020-10-04 12:40:38 -0700

    OFFLINE CODING SEVERAL HOURS BIG UPDATE
    you code better when there's no distractions :D

--- currency.py
+++ currency.py
@@ -6,53 +6,84 @@
 # values from: 
 # https://www1.oanda.com/currency/comparison
 
-# values for converting
-# convert 1 USD to -> 
-australian_dollar = 1.4003
-brazilian_real = 5.6274
-british_pound = 0.77683
-canadian_dollar = 1.33632
-chinese_yuan = 6.80127
-danish_krone = 6.34950
-euro = 0.85284
-hong_kong_dollar = 7.74988
-hungarian_forint = 310.68414
-indian_rupee = 73.56519
-indonesian_rupiah = 14897.42782
-japanese_yen = 105.57228
-mexican_peso = 22.25874
-new_zealand_dollar = 1.51507
-norwegian_kroner = 9.40294
-polish_zloty = 3.86343
-russian_rouble = 78.24798
-saudi_riyal = 3.74933
-singapore_dollar = 1.36727
-south_african_rand = 16.82467
-swedish_krona = 8.97499
-swiss_franc = 0.92104
-taiwan_dollar = 28.95307
-thai_baht = 31.59357
-turkish_lira = 7.75733
-us_dollar = 1
-
 
 def main(): 
+    print(values)
     que = int(input("Helo, please enter a number of what you would like to do today: \n1) exchange currencies \n2) view exchange rates \n"))
     if que == 1:
-        pass
+        exchange_currency()
     elif que == 2:
         print_currencies()
 
-    '''
-    user_input = float(input("How many USD to TWD? "))
-    TWD = 28.95307
-    output = user_input * TWD
-    output = round(output, 2)     # change decimal to two points
-    print(str(user_input) + " USD was converted to: " + str(output) + " TWD. \nThank you for using our service.")
-    '''
 
 
-# when called upon print list of available currencies and values
+# values for converting
+# value equivalent of 1 USD
+# put values into dictionary so can call easily
+values = dict()
+values["AUD"] = 1.4003
+values["BRL"] = 5.6274
+values["GBP"] = 0.77683
+values["CAD"] = 1.33632
+values["CNY"] = 6.80127
+values["DKK"] = 6.34950
+values["EUR"] = 0.85284
+values["HKD"] = 7.74988
+values["HUF"] = 310.68414
+values["INR"] = 73.56519
+values["IDR"] = 14897.42782
+values["JPY"] = 105.57228
+values["MXN"] = 22.25874
+values["NZD"] = 1.51507
+values["NOK"] = 9.40294
+values["PLN"] = 3.86343
+values["RUB"] = 78.24798
+values["SAR"] = 3.74933
+values["SGD"] = 1.36727
+values["ZAR"] = 16.82467
+values["SEK"] = 8.97499
+values["CHF"] = 0.92104
+values["TWD"] = 28.95307
+values["THB"] = 31.59357
+values["TRY"] = 7.75733
+values["USD"] = 1
+
+# Get user input to see which currencies they would like to exchange
+def exchange_currency():
+    print("(if you do not know your exchange symbol, please enter \"help\" to refer to the exchange rates page)")
+    before_currency = input("Please enter the Exchange Symbol of the currency you wish to exchange: ")
+    if before_currency.lower() == "help":
+        print_currencies()
+    else: 
+
+        if before_currency in values:
+            exchange_before = values[before_currency]
+        else: 
+            print("Error: Exchange Symbol not found or not supported. ")
+            exchange_currency()
+        before_value = int(input("How much " + str(before_currency) + " do you wish to exchange? "))
+        # ^ ASSUMES INPUT WILL BE INTEGER TRY EXCEPT CHECK IF INT OTHERWISE PRINT ERROR MESSAGE
+        
+
+        after_currency = input("Please enter the Exchange Symbol of the currency you wish to receive: ")
+        if after_currency in values:
+            exchange_after = values[after_currency]
+        else: 
+            print("Error: Exchange Symbol not found or not supported. ")
+
+        after_value = before_value // exchange_before * exchange_after
+        after_value = round(after_value, 2)
+        print(str(before_value) + " " + str(before_currency) + " was converted to " + str(after_value) + " " + str(after_currency))
+        print("Thank you for using this service, we hope to see you soon! ")
+
+
+
+
+
+
+
+
+# Print list of available currencies and values as well as their currency exchange symbols
 def print_currencies(): 
     print("\n" + '\033[1m' + '\033[92m' + "Currency Exchange Rates: " + '\033[0m' + 
   """
